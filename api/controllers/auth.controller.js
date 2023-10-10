d5fc68224bdbe194e82e047905e604fce15342bf
Krishna Kumar <krishnassm420012@gmail.com>
2023-10-10 23:38:38 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
-import User from "../model/user.model.js";
 import bcryptjs from 'bcryptjs';
+import User from './../model/user.model.js';
+import { errorHandler } from './../utils/error.js';
+import jwt from 'jsonwebtoken';
 export const signup=async(req,res,next)=>{
     const{username,email,password}=req.body;
     const hashedPassword=await bcryptjs.hashSync(password,10);
@@ -15,3 +17,24 @@ export const signup=async(req,res,next)=>{
         next(error);
     }
 } 
+
+export const signin=async(req,res,next)=>{
+    const{email,password}=req.body;
+    try {
+        const validUser=await User.findOne({email});
+        if(!validUser){
+            next(errorHandler(404),'User Not Found!')
+        }
+        const validPassword=bcryptjs.compareSync(password,validUser.password);
+        if(!validPassword){
+            next(errorHandler(401,'Invalid Password'));
+        }
+        const{password:pass,...rest}=validUser._doc
+
+        const token=jwt.sign({id:validUser._id},process.env.JWT_SECRET);
+        res.cookie('access_token',token,{httpOnly:true}).status(200).json(rest);
+        
+    } catch (error) {
+        next(error)
+    }
+}
