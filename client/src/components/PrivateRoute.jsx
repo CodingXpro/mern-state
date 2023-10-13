2f3fc0b4da8b54a358c212f784f6429034e6992a
Krishna Kumar <krishnassm420012@gmail.com>
2023-10-14 01:39:47 +0530

    Complete profile page UI

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { Outlet,Navigate } from 'react-router-dom'
 
-export const PrivateRoute = () => {
+export default function PrivateRoute () {
     const{currentUser}=useSelector((state)=>state.user);
     
 
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 export default function Profile() {
+  const{currentUser}=useSelector((state)=>state.user);
   return (
-    <div>Profile</div>
+   <div className='max-w-lg p-3 mx-auto'>
+    <h1 className='text-center font-semibold text-3xl my-7'>
+      Profile
+    </h1>
+    <form className='flex flex-col gap-4'>
+      <img src={currentUser.avatar} alt='profile' className='rounded-full h-24 w-24 object-cover self-center cursor-pointer mt-2 '/>
+      <input placeholder='username' id='username' type='text' className='border p-3 rounded-lg'/>
+        <input placeholder='email' id='email' type='text' className='border p-3 rounded-lg'/>
+         <input placeholder='password' id='password' type='text' className='border p-3 rounded-lg'/>
+         <button className='bg-slate-700 p-3 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>update</button>
+    </form>
+    <div className='flex justify-between mt-5'>
+      <span className='text-red-700'>Delete Account</span>
+      <span className='text-red-700'>Sign out</span>
+    </div>
+   </div>
   )
 }
+
+ 
