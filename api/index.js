6d8c2316e9a8003183ce82760eff66dc81add72f
Krishna Kumar <krishnassm420012@gmail.com>
2023-11-15 01:39:16 +0530

    create update user api route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 import userRoutes from "./routes/user.route.js";
 import authRoutes from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 
 
 mongoose.connect(process.env.MONGO_URL).then(()=>{
@@ -13,6 +14,7 @@ mongoose.connect(process.env.MONGO_URL).then(()=>{
 })
 const app=express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/user',userRoutes)
 app.use('/api/auth',authRoutes);
